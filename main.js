76337ca8dcd6e3968ce716b6a9613b4cc294987e
Arya shibu <arya.shibu.dhanya1200@gmail.com>
2025-02-02 20:28:47 +0530

    commit

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-const todolist= document.getElementsByClassName("todolist");
-const list= document.getElementsByClassName("list");
-const input= document.getElementsByTagName("input");
+const container= document.getElementById("list-container");
+const input= document.getElementById("input");
+
+function addtask(){
+    if (input.value=== ""){
+        alert("you must write something!!");}
+    else{
+        let p = document.createElement("p");
+        let checkbox= document.createElement("input");
+        checkbox.type="checkbox";
+        p.innerText=input.value;
+        p.insertBefore(checkbox, p.firstChild);
+        container.appendChild(p);
+        checkbox.style.marginRight= "10px";
+
+        input.value="";
+       
+    }
+    }
+
+
 
